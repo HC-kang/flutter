3651a17f61fe129023f3eb771ce603eb2c8b8829
HC-kang <weston0713@gmail.com>
2021-07-20 20:18:31 +0900

    0720

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -26,7 +26,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _biggerFont = const TextStyle(fontSize: 33.0);
   @override
   void _pushSaved() {
     Navigator.of(context).push(
